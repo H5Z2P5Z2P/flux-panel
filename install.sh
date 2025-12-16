bc77d3e4ca0a065c0775165871ad619ca6601abb
huaihuai <sfly217@gmail.com>
2025-12-16 22:11:42 +0800

    Update install script URL and backend logic

--- install.sh
+++ install.sh
@@ -19,17 +19,18 @@ get_architecture() {
 # 构建下载地址
 build_download_url() {
     local ARCH=$(get_architecture)
-    echo "https://github.com/bqlpfy/flux-panel/releases/download/1.4.3/gost-${ARCH}"
+    # echo "https://github.com/bqlpfy/flux-panel/releases/download/1.4.3/gost-${ARCH}"
+    echo "https://minio.uily.de/files/flux-agent/flux-agent-linux-${ARCH}"
 }
 
 # 下载地址
 DOWNLOAD_URL=$(build_download_url)
 INSTALL_DIR="/etc/gost"
-COUNTRY=$(curl -s https://ipinfo.io/country)
-if [ "$COUNTRY" = "CN" ]; then
-    # 拼接 URL
-    DOWNLOAD_URL="https://ghfast.top/${DOWNLOAD_URL}"
-fi
+# COUNTRY=$(curl -s https://ipinfo.io/country)
+# if [ "$COUNTRY" = "CN" ]; then
+#     # 拼接 URL
+#     DOWNLOAD_URL="https://ghfast.top/${DOWNLOAD_URL}"
+# fi
 
 
 
